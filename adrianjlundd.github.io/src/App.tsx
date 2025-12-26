a6574511e0fb08e2314901e7500f227970657a58
Adrian Jacobsen Lund <adriajl@stud.ntnu.no>
2025-12-26 22:01:57 +0100

    fix: missing  opening jfx argument

--- adrianjlundd.github.io/src/App.tsx
+++ adrianjlundd.github.io/src/App.tsx
@@ -1,7 +1,6 @@
 export default function App() {
   return (
-
-      <main>
+    <main>
       <h1 className="text-2xl font-bold">Portfolio - Adrian Jacobsen Lund</h1>
       <h2 className="text-xl mt-4">Computer Science student @ NTNU</h2>
     </main>
